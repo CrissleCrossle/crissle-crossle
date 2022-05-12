b4f641fb60347fd3ec8bee0f5149bc461b4d70ee
CrissleCrossle <105291171+CrissleCrossle@users.noreply.github.com>
2022-05-12 13:27:29 +0100

    Added button for modes + google analytics

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+document.getElementById("how").onclick = () => {
+  console.log("what")
+}
+
 let tempInterfere = targetWords
 
 targetWord = targetWords[dayOffset]
@@ -5,7 +10,7 @@ targetWord = targetWords[dayOffset]
 tempInterfere.splice(dayOffset, 1)
 
 for (let i = 0; i < 7; i++) {
-  let index = Math.floor(mulberry32(parseInt(String(dayOffset)+"1111"+String(i)))()*2315)
+  let index = Math.floor(mulberry32(parseInt(String(dayOffset)+"1111"+String(i)))()*(2315-i))
   let word = tempInterfere[index]
   tempInterfere.splice(index, 1)
   interfereWords.push(word)
--- infinite.js
+++ infinite.js
@@ -7,7 +7,7 @@ targetWord = targetWords[i]
 tempInterfere.splice(i, 1)
 
 for (let i = 0; i < 7; i++) {
-  let index = Math.floor(Math.random()*2315)
+  let index = Math.floor(Math.random()*(2315-i))
   let word = tempInterfere[index]
   tempInterfere.splice(index, 1)
   interfereWords.push(word)
