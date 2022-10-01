d99b47d67bbc909b63ed28879e8cde73c03ab542
Iddmeister <iddonagenda.t@gmail.com>
2022-10-01 16:57:50 +0100

    toki pona

---
+++ tp/infinite.js
@@ -0,0 +1,31 @@
+saveAfterGuess = false
+
+let tempInterfere = targetWords
+
+var i = Math.floor(Math.random()*67)
+targetWord = targetWords[i]
+tempInterfere.splice(i, 1)
+
+for (let i = 0; i < 7; i++) {
+  let index = Math.floor(Math.random()*(67-i))
+  let word = tempInterfere[index]
+  tempInterfere.splice(index, 1)
+  interfereWords.push(word)
+}
+
+interfereWords.push("#####")
+
+startInteraction()
+
+document.getElementById("restart").onclick = document.getElementById("re").onclick = () => {
+
+  location.reload()
+
+}
+
+win = lose = () => {
+  document.getElementById("hide-keyboard").style.visibility = "visible"
+  document.getElementById("restart").style.visibility = "visible"
+
+      
+}
